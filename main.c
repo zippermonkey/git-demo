583e460a6879b43c6f3e0315c5cb69c59bad41f3
xingbaoyue31435 <xing.baoyue@h3c.com>
2026-01-09 17:55:37 +0800

    count c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
-int main() {
-	printf("hello, git\n");
-	printf("hello, world\n");
-	printf("lalala");
+
+/*count digits\white space\others */
+int main(){
+	int c, i, nwhite, others;
+	int ndigits[10];
+
+	nwhite = others = 0;
+
+	for (i = 1; i < 10; i++){
+		ndigits[i] = 0;
+	}
+
+	while((c = getchar()) != EOF){
+		if (c >= '0' && c <= '9'){
+			ndigits[c-'0']++;
+		}
+		else if (c == ' ' || c == '\n' || c == '\t')
+		{
+			/* code */
+			nwhite++;
+		}
+		else{
+			others++;
+		}
+	}
+	printf("digits =");
+	for (i = 0; i < 10; i++){
+		printf(" %d", ndigits[i]);
+	}
+	printf(", white space = %d, other = %d\n", nwhite, others);
+
 }
